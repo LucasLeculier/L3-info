65c01ac5ce11cc8948a218adcb40b0d61242ba7f
LucasLeculier <102011516+LucasLeculier@users.noreply.github.com>
2022-09-27 11:26:25 +0200

    Update DeMemoire.java

--- atelier/Atelier1 POO/Atelier1_ex1/DeMemoire.java
+++ atelier/Atelier1 POO/Atelier1_ex1/DeMemoire.java
@@ -4,37 +4,36 @@ public class DeMemoire extends De {
 	
 	int valeur;
 	
-	public DeMemoire() {
-	this.nbFaces=6;
-	this.nom="Dé"+nbDes;
-	nbDes++;
-	}
-	
       public DeMemoire(String nom,int nbFaces) {
 		if (nbFaces >= 3 && nbFaces <= 120) {
 			this.nbFaces = nbFaces;
 		}
 		else {
 			//System.out.println("Nombre de faces impossible, nombre de faces établi a 6");
-			this.nbFaces = 6;
+			this.nbFaces = defaultValue;
 		}
 		this.nom=nom;
 		nbDes++;
 }
 	public DeMemoire(String nom) {
-		this(nom,6);
+		this(nom,defaultValue);
 		
 	}
 	public DeMemoire(int nbFaces) {
-		this("de"+nbDes)
+		this("de"+nbDes,nbFaces)
 }
+	public DeMemoire() {
+	this("de"+defaultValue);
+	}
 	
 	public int lancer() {
 		int newValeur;
 		while(true) {
 			newValeur =r.nextInt(1,this.nbFaces + 1);
+			if(newValeur==nbFaces){
+				return newValeur;//Cas ou la valeur tirée est le max
 			if (newValeur != valeur) {
-				valeur = newValeur;
+				valeur = newValeur; 
 				break;
 			}
 		}
